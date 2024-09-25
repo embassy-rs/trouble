d188bfddd071eac61de69e13d47095d19af0ac78
Ulf Lilleengen <ulf@digili.no>
2024-09-25 09:26:48 +0200

    Use cargo batch and caching in CI

---
+++ ci.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -eo pipefail
+
+if ! command -v cargo-batch &> /dev/null; then
+    echo "cargo-batch could not be found. Install it with the following command:"
+    echo ""
+    echo "    cargo install --git https://github.com/embassy-rs/cargo-batch cargo --bin cargo-batch --locked"
+    echo ""
+    exit 1
+fi
+
+export RUSTFLAGS=-Dwarnings
+export DEFMT_LOG=trace
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+if [[ -z "${CARGO_TARGET_DIR}" ]]; then
+    export CARGO_TARGET_DIR=target_ci
+fi
+
+cargo batch \
+    --- build --release --manifest-path examples/nrf-sdc/Cargo.toml --target thumbv7em-none-eabihf --features nrf52840 \
+    --- build --release --manifest-path examples/nrf-sdc/Cargo.toml --target thumbv7em-none-eabihf --features nrf52832 \
+    --- build --release --manifest-path examples/esp32/Cargo.toml --target riscv32imc-unknown-none-elf \
+    --- build --release --manifest-path examples/serial-hci/Cargo.toml \
+    --- build --release --manifest-path host/Cargo.toml --features gatt,peripheral,central \
+    --- build --release --manifest-path examples/rp-pico-w//Cargo.toml --target thumbv6m-none-eabi --features skip-cyw43-firmware \
+    --- build --release --manifest-path examples/apache-nimble/Cargo.toml --target thumbv7em-none-eabihf
+
+cargo fmt --check --manifest-path ./host/Cargo.toml
+cargo clippy --manifest-path ./host/Cargo.toml --features gatt,peripheral,central
+cargo test --manifest-path ./host/Cargo.toml --lib -- --nocapture
