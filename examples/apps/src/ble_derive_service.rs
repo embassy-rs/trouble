288996bf51800ae5cbed72b32d7a693e0eba1d95
James Sizeland <james.sizeland@dyson.com>
2024-10-03 23:09:35 +0100

    remove descriptors

--- examples/apps/src/ble_derive_service.rs
+++ examples/apps/src/ble_derive_service.rs
@@ -2,16 +2,16 @@ use trouble_host::prelude::*;
 
 #[gatt_service(uuid = "0x180f")]
 struct HeartRateService {
-   #[characteristic(uuid = "0x2A37", read, notify)]
-//    #[descriptor(uuid = "0x2902", value = "heart rate in bpm")]
-   rate: f32,
-   #[characteristic(uuid = "0x2A38", read)]
-//    #[descriptor(uuid = "0x2902", value = "body sensor location")]
-   location: f32,
-   #[characteristic(uuid = "0x2A39", write)]
-//    #[descriptor(uuid = "0x2902", value = "heart rate control point")]
-   control: u8,
-   #[characteristic(uuid = "0x2A63", read, notify)]
-//    #[descriptor(uuid = "0x2902", value = "energy expended")]
-   energy_expended: u16,
+    #[characteristic(uuid = "0x2A37", read, notify)]
+    rate: f32,
+    #[characteristic(uuid = "0x2A38", read)]
+    location: f32,
+    #[characteristic(uuid = "0x2A39", write)]
+    control: u8,
+    #[characteristic(uuid = "0x2A63", read, notify)]
+    energy_expended: u16,
+}
+
+fn main() {
+    let service = HeartRateService::new().unwrap();
 }
