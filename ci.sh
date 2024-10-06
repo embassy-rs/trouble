29485b1b1b46bdf1329cc2533ff70edc145aae65
gibbz00 <gabrielhansson00@gmail.com>
2024-10-06 15:00:58 +0200

    Initialize a cargo workspace
    Makes it easier to run most cargo commands.

--- ci.sh
+++ ci.sh
@@ -18,14 +18,14 @@ cargo batch \
     --- build --release --manifest-path examples/nrf-sdc/Cargo.toml --target thumbv7em-none-eabihf --features nrf52832 \
     --- build --release --manifest-path examples/esp32/Cargo.toml --target riscv32imc-unknown-none-elf \
     --- build --release --manifest-path examples/serial-hci/Cargo.toml \
-    --- build --release --manifest-path host/Cargo.toml --features peripheral \
-    --- build --release --manifest-path host/Cargo.toml --features central \
-    --- build --release --manifest-path host/Cargo.toml --features gatt,peripheral \
-    --- build --release --manifest-path host/Cargo.toml --features gatt,central \
-    --- build --release --manifest-path host/Cargo.toml --features gatt,peripheral,central,scan \
-    --- build --release --manifest-path examples/rp-pico-w//Cargo.toml --target thumbv6m-none-eabi --features skip-cyw43-firmware
-#    --- build --release --manifest-path examples/apache-nimble/Cargo.toml --target thumbv7em-none-eabihf
+    --- build --release --manifest-path examples/rp-pico-w//Cargo.toml --target thumbv6m-none-eabi --features skip-cyw43-firmware \
+    --- build --release -p trouble-host --features peripheral \
+    --- build --release -p trouble-host --features central \
+    --- build --release -p trouble-host --features gatt,peripheral \
+    --- build --release -p trouble-host --features gatt,central \
+    --- build --release -p trouble-host --features gatt,peripheral,central,scan
+#   --- build --release --manifest-path examples/apache-nimble/Cargo.toml --target thumbv7em-none-eabihf
 
-cargo fmt --check --manifest-path ./host/Cargo.toml
-cargo clippy --manifest-path ./host/Cargo.toml --features gatt,peripheral,central
-cargo test --manifest-path ./host/Cargo.toml --lib -- --nocapture
+cargo fmt --check
+cargo clippy --features gatt,peripheral,central
+cargo test -- --nocapture
