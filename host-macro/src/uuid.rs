d01fde7040be3dbdfb73433c81c0e25f204e08a7
Pete Kubiak <peter.kubiak@dyson.com>
2024-10-08 17:14:06 +0100

    Improve UUID parsing from string
    Co-authored-by: Scott Gibb <Scott.Gibb@dyson.com>
    Co-authored-by: James Sizeland <James.Sizeland@dyson.com>

--- host-macro/src/uuid.rs
+++ host-macro/src/uuid.rs
@@ -16,8 +16,7 @@ pub enum Uuid {
 impl FromMeta for Uuid {
     fn from_string(value: &str) -> darling::Result<Self> {
         if let Ok(u) = uuid::Uuid::from_str(value) {
-            let mut bytes = *u.as_bytes();
-            bytes.reverse(); // Softdevice uses uuids in little endian format.
+            let bytes = u.to_bytes_le(); // Little-endian, as per Bluetooth spec
             return Ok(Uuid::Uuid128(bytes));
         }
 
