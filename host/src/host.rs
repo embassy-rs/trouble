4ed1da86f1d0edf482be4090a34c39e1ebda81ad
Ulf Lilleengen <ulf@digili.no>
2024-10-15 13:08:00 +0200

    log panic

--- host/src/host.rs
+++ host/src/host.rs
@@ -684,7 +684,7 @@ impl<'d, C: Controller> RxRunner<'d, C> {
                                 }
                                 (Ok(handle), Err(e)) => {
                                     warn!("[host] error processing completed packets for {:?}: {:?}", handle, e);
-                                    panic!(e);
+                                    panic!("{}", e);
                                 }
                                 _ => {}
                             }
