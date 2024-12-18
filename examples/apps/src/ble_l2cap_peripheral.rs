4c57d3bc6f94ad2be4cd676609389daa9576bb5d
bytedream <bytedream@protonmail.com>
2024-12-18 18:41:15 +0100

    Add more esp32 examples

--- examples/apps/src/ble_l2cap_peripheral.rs
+++ examples/apps/src/ble_l2cap_peripheral.rs
@@ -9,7 +9,11 @@ const L2CAP_TXQ: u8 = 20;
 const L2CAP_RXQ: u8 = 20;
 
 /// Size of L2CAP packets
-const L2CAP_MTU: usize = 27;
+#[cfg(not(feature = "esp"))]
+const L2CAP_MTU: usize = 128;
+#[cfg(feature = "esp")]
+// Some esp chips only accept an MTU >= 1017
+const L2CAP_MTU: usize = 1017;
 
 /// Max number of connections
 const CONNECTIONS_MAX: usize = 1;
