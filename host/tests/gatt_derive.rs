0e82913d22862fa752aed1ccc998389ae0b1a233
Preston Peranich <pperanich@gmail.com>
2025-01-03 13:17:48 -0500

    fix: initialize storage arrays to max size.

--- host/tests/gatt_derive.rs
+++ host/tests/gatt_derive.rs
@@ -16,7 +16,7 @@ const VALUE_UUID: Uuid = Uuid::new_long([
     0x00, 0x00, 0x10, 0x01, 0xb0, 0xcd, 0x11, 0xec, 0x87, 0x1f, 0xd4, 0x5d, 0xdf, 0x13, 0x88, 0x40,
 ]);
 
-#[gatt_server(mutex_type = NoopRawMutex, attribute_table_size = 22)]
+#[gatt_server(mutex_type = NoopRawMutex, attribute_table_size = 25)]
 struct Server {
     service: CustomService,
 }
@@ -40,6 +40,8 @@ struct CustomService {
     pub third: [u8; 2],
     #[characteristic(uuid = "408816df-5dd4-1f87-ec11-cdb001100000", read, write, notify)]
     pub fourth: heapless::Vec<u8, 3>,
+    #[characteristic(uuid = "408816df-5dd4-1f87-ec11-cdb001100000", read, write, notify)]
+    pub fifth: heapless::Vec<u8, 33>,
 }
 
 #[tokio::test]
