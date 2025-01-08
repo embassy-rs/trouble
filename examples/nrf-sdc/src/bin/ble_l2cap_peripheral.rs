ad38b523407a93451949119a45ec595dfa67e6f4
Ulf Lilleengen <ulf@digili.no>
2025-01-08 16:16:15 +0100

    Make code compile

--- examples/nrf-sdc/src/bin/ble_l2cap_peripheral.rs
+++ examples/nrf-sdc/src/bin/ble_l2cap_peripheral.rs
@@ -72,7 +72,6 @@ async fn main(spawner: Spawner) {
 
     let mut sdc_mem = sdc::Mem::<12848>::new();
     let sdc = unwrap!(build_sdc(sdc_p, &mut rng, mpsl, &mut sdc_mem));
-    panic!("Uh oh");
 
     ble_l2cap_peripheral::run(sdc).await;
 }
