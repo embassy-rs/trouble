cf2b43a7b42b253368610f7e06af9ba1bc6970dd
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2025-01-17 19:46:02 +0100

    bump mtu for esp32c3

--- examples/esp32/src/consts.rs
+++ examples/esp32/src/consts.rs
@@ -1,6 +1,6 @@
 /// Size of L2CAP packets
 #[cfg(not(any(feature = "esp32c2", feature = "esp32c6", feature = "esp32h2")))]
-pub const L2CAP_MTU: usize = 128;
+pub const L2CAP_MTU: usize = 251;
 // Some esp chips only accept an MTU >= 255
 #[cfg(any(feature = "esp32c2", feature = "esp32c6", feature = "esp32h2"))]
 pub const L2CAP_MTU: usize = 255;
