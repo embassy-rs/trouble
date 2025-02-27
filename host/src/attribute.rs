55819aa5839ce954d9b14b895e1f1d8149b16232
Pete Kubiak <peter.kubiak@dyson.com>
2025-02-27 11:41:00 +0000

    Format with the correct version of rustfmt

--- host/src/attribute.rs
+++ host/src/attribute.rs
@@ -5,17 +5,17 @@ use core::marker::PhantomData;
 
 use bt_hci::uuid::declarations::{CHARACTERISTIC, PRIMARY_SERVICE};
 use bt_hci::uuid::descriptors::CLIENT_CHARACTERISTIC_CONFIGURATION;
-use embassy_sync::blocking_mutex::raw::RawMutex;
 use embassy_sync::blocking_mutex::Mutex;
+use embassy_sync::blocking_mutex::raw::RawMutex;
 use heapless::Vec;
 
+use crate::Error;
 use crate::att::AttErrorCode;
 use crate::attribute_server::AttributeServer;
 use crate::cursor::{ReadCursor, WriteCursor};
 use crate::prelude::{AsGatt, Connection, FixedGattValue, FromGatt};
 use crate::types::gatt_traits::FromGattError;
 pub use crate::types::uuid::Uuid;
-use crate::Error;
 
 /// Characteristic properties
 #[derive(Debug, Clone, Copy)]
