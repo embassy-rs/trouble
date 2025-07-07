1e6f2d743857ab6888a8a59199ad0c186bbf38c7
Logan Pairman <logan@glasson.nz>
2025-07-07 15:07:11 +1200

    feat(examples): Add example using Linux HCI sockets

---
+++ examples/linux/src/bin/ble_beacon.rs
@@ -0,0 +1,19 @@
+use bt_hci::controller::ExternalController;
+use trouble_example_apps::ble_beacon;
+use trouble_linux_examples::Transport;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+    let dev = match std::env::args().collect::<Vec<_>>()[..] {
+        [_] => 0,
+        [_, ref s] => s.parse::<u16>().expect("Could not parse device number"),
+        _ => panic!(
+            "Provide the device number as the one and only command line argument, or no arguments to use device 0."
+        ),
+    };
+    let transport = Transport::new(dev)?;
+    let controller = ExternalController::<_, 8>::new(transport);
+    ble_beacon::run(controller).await;
+    Ok(())
+}
