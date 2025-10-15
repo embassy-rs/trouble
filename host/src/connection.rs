c87f5cb24f03f0eec7769f8e49bf4697d056d7be
Haobo Gu <haobogu@outlook.com>
2025-10-15 22:45:55 +0800

    fix: enabled l2cap connection parameter update req for peripheral
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- host/src/connection.rs
+++ host/src/connection.rs
@@ -24,7 +24,7 @@ use crate::prelude::{AttributeServer, GattConnection};
 #[cfg(feature = "security")]
 use crate::security_manager::{BondInformation, PassKey};
 #[cfg(feature = "connection-params-update")]
-use crate::types::l2cap::{ConnParamUpdateReq, ConnParamUpdateRes};
+use crate::types::l2cap::ConnParamUpdateRes;
 use crate::{bt_hci_duration, BleHostError, Error, Identity, PacketPool, Stack};
 
 /// Security level of a connection
@@ -480,8 +480,8 @@ impl<'stack, P: PacketPool> Connection<'stack, P> {
             }
         }
 
-        #[cfg(feature = "connection-params-update")]
-        {
+        if self.role() == LeConnRole::Peripheral || cfg!(feature = "connection-params-update") {
+            use crate::types::l2cap::ConnParamUpdateReq;
             // Use L2CAP signaling to update connection parameters
             info!(
                 "Connection parameters request procedure not supported, use l2cap connection parameter update req instead"
