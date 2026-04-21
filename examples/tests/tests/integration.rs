f60fec45b4c15d3bf6c34ba791c69cfe4ebfd1cd
Ulf Lilleengen <ulf@digili.no>
2026-04-21 11:33:38 +0200

    fix: single module for tests

---
+++ examples/tests/tests/integration.rs
@@ -0,0 +1,6 @@
+#[path = "integration/ble_bas_peripheral.rs"]
+mod ble_bas_peripheral;
+#[path = "integration/ble_l2cap_central.rs"]
+mod ble_l2cap_central;
+#[path = "integration/ble_l2cap_peripheral.rs"]
+mod ble_l2cap_peripheral;
